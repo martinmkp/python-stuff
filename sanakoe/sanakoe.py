2add74ebc5433bb07d0be6bdf3978a8d7cfbec82
martinmkp <martin.makipaa@gmail.com>
2022-01-15 16:40:13 +0200

    update

--- sanakoe/sanakoe.py
+++ sanakoe/sanakoe.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 def sanakoe(sanasto):
     sanat = pd.read_excel(sanasto, header = 0)
-    sanat_vieras = list(sanat.iloc[:, 0])
-    sanat_suomi = list(sanat.iloc[:, 1])
+    sanat_1 = list(sanat.iloc[:, 0])
+    sanat_2 = list(sanat.iloc[:, 1])
 
     pisteet = 0
     maksimi_pisteet = sanat.shape[0]
 
     for i in range(maksimi_pisteet):
-        sana = input(f"Sana {i+1}/{maksimi_pisteet}: {sanat_vieras[i]}: ")
-        if sana == sanat_suomi[i]:
+        sana = input(f"Sana {i+1}/{maksimi_pisteet}: {sanat_1[i]}: ")
+        if sana == sanat_2[i]:
             print("Oikein!")
             pisteet +=1
         else:
-            print(f"Väärin. Oikea vastaus: {sanat_suomi[i]}")
+            print(f"Väärin. Oikea vastaus: {sanat_2[i]}")
     print(f"Tulos: {pisteet}/{maksimi_pisteet}")
